ce3e95328d4d1e30217bb93587f40a31406c1e48
erfjab <erfjab@gmail.com>
2025-08-10 04:30:18 +0330

    docker

--- src/handlers/servers/create.py
+++ src/handlers/servers/create.py
@@ -22,7 +22,7 @@ class ServerCreateForm(StateGroup):
 async def servers_create(callback_query: CallbackQuery, db: AsyncSession, state: StateManager, state_data: dict):
     await state.upsert_context(db=db, state=ServerCreateForm.remark)
     return await callback_query.message.edit(
-        text=Dialogs.SERVERS_ENTER_REMARK, reply_markup=BotKB.servers_back(state_data["client_id"])
+        text=Dialogs.SERVERS_ENTER_REMARK, reply_markup=BotKB.servers_back()
     )
 
 
